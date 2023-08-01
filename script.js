573937631db83d49e956309173608b98525af9f7
Darlington Daniel <58652962+darlingdaniel4790@users.noreply.github.com>
2023-08-01 17:54:22 +0300

    fixed z-index for back to top button

--- script.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.getElementById("mobileMenu");
-const navLinks = document.getElementById("navLinks");
-
-mobileMenu.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
